81fd131c8ed78b56ec5919b76f4d828ebd306bd1
sawer <khatynski7828921@gmail.com>
2026-06-07 19:37:17 +0300

    lab2 wa added

---
+++ lab2/triangle.js
@@ -0,0 +1,4 @@
+let katet1=parseFloat(prompt("Введите первый катет"));
+let katet2=parseFloat(prompt("Введите второй катет"));
+let hypo=Math.sqrt(Math.pow(katet1,2)+Math.pow(katet2,2));
+alert("Гипотенуза равна: "+ hypo);
